a12a45ab8dacf8b78440cb9e7fe59db1aad36a03
HatmanStack <hatmanstack@protonmail.com>
2023-09-28 13:14:13 -0500

    Zappa Update

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# snow-flask-whoami-aws
+# Who Am I : AWS
+
+This is the AWS version of Who Am I.  It uses AWS Lambda and AWS API Gateway to build a microservice with Flask that provides an API for querying information in a Snowflake Database.  The information is displayed with interactive Vega-Lite visualizations.
+
+## Technologies
+
+The service is built and deployed automatically using [Zappa](https://github.com/zappa/Zappa) a python library used for deploying serverless applications to AWS Lambda.  It automatically creates a s3 bucket, execution role, API Gateway instance, and Lambda Function.  It is necessary to add your snowflake database information as an enviromental variable in the zappa.config file for the snowflake-connector-python. 
+
+## Deployment
+
+To run:
+
+```
+python -m venv venv
+
+Mac/Linux:
+source /venv/bin/activate
+
+Windows:
+.\venv\scripts\activate
+
+pip install -r requirements.txt
+zappa init
+```
+
+Select the defaults and add this line to zappa_settings.json replaceing `Snowflake-Region`, `Snowflake-Username`, `Snowflake-Passowrd` with your own Snowflake credentials.
+
+```
+"aws_environment_variables" : {"REGION": "<Snowflake-Region>" ,
+         "PASSWORD": "<Snowflake-Password>", "USERNAME": "<Snowflake-Username>"}
+```
+
+
+The service is now built and deployed.  If you'd like to make changes use `zappa update`.
---
+++ main.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, request
+from snowflake import connector
+import pandas as pd
+import os
+
+
+app = Flask(__name__)
+
+stage_name = request.path.split('/')[1] #Can sometimes be different if you run into troubles try hardcoding
+
+@app.route('/')
+def homepage():
+    cur = cnx.cursor().execute("Select Name, count(*) from ADDRESSES group by NAME;")
+    data4charts=pd.DataFrame(cur.fetchall(), columns=['NAME','vote'])
+    data4chartsJSON = data4charts.to_json(orient='records')
+    return render_template('charts.html', data4chartsJSON=data4chartsJSON)
+
+@app.route(f'/{stage_name}/submit')
+def submitpage():
+    return render_template('submit.html')
+
+@app.route(f'/{stage_name}/harddata')
+def hardData():
+    dfhtml = updateRows().to_html()
+    return render_template('index.html', dfhtml=dfhtml)
+
+@app.route(f'/{stage_name}/thanks4submit', methods=["POST"])
+def thanks4submit():
+    address = request.form.get("cname")
+    name = request.form.get("uname")
+    insertRow(address, name)
+    return render_template('thanks4submit.html',
+                           colorname=address,
+                           username=name)
+    
+
+cnx = connector.connect(
+    account= os.environ.get('REGION'),
+    user= os.environ.get('USERNAME'),
+    password= os.environ.get('PASSWORD'),
+    warehouse='COMPUTE_WH',
+    database='DEMO_DB',
+    schema='PUBLIC',
+    role='ACCOUNTADMIN'
+)
+
+
+def insertRow(address, name):
+    cur = cnx.cursor()
+    updateString = "INSERT INTO ADDRESSES(ADDRESS, NAME) VALUES ('{}', '{}')".format(address, name)
+    print(updateString)
+    cur.execute(updateString)
+
+def updateRows():
+    cur = cnx.cursor()
+    cur.execute("SELECT * FROM ADDRESSES")
+    rows = pd.DataFrame(cur.fetchall(),columns=['ADDRESS', 'NAME'])
+    return rows
+
+
+if __name__ == '__main__': 
+    app.run()
